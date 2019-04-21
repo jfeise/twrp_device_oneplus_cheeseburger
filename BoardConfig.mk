4ce2d37432653a70933611a583647cb8a6cb56e5
engstk <eng.stk@sapo.pt>
2019-04-21 19:16:26 +0000

    Add some new twrp dependencies and couple fixes
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,8 @@ TARGET_USES_UEFI := true
 # Crypto
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := false
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 
@@ -93,13 +95,14 @@ BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := "26"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
-TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
 
 # Init properties from bootloader version
 TARGET_INIT_VENDOR_LIB := libinit_cheeseburger
@@ -108,5 +111,5 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_cheeseburger
 #Extra
 BOARD_SUPPRESS_SECURE_ERASE := true
 TW_IGNORE_MISC_WIPE_DATA := true
-TW_DEFAULT_BRIGHTNESS := "26"
-TW_USE_TOOLBOX := true
+TW_EXCLUDE_TWRPAPP := true
+TW_HAS_EDL_MODE := true
