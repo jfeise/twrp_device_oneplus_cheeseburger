bb2811f9d03bc238b27df5d3b5cc6d484eaf670b
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-04-21 08:13:00 +0200

    cheeseburger: update for pie
    Change-Id: Icc3c411b7447d90fe4d3402e7bfd286d6bb33d07
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- omni_cheeseburger.mk
+++ omni_cheeseburger.mk
@@ -25,9 +25,18 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Set by init module based on ro.boot.rf_version argument from bootloader
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.device ro.product.model ro.build.product 
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8998 \
+    ro.hardware.gatekeeper=msm8998
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_NAME := omni_cheeseburger
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus A5000
 PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus5-user 9 PKQ1.180716.001 1902221944 release-keys"
+
+BUILD_FINGERPRINT := OnePlus/OnePlus5/OnePlus5:9/PKQ1.180716.001/1902221944:user/release-keys
